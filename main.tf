101086986aa83da2dc7d8334c82fd98f19da1d2c
ssilva1 <silvasergio@live.com.ar>
2026-08-24 19:45:44 -0300

    Configurado backend remoto en S3

--- main.tf
+++ main.tf
@@ -5,14 +5,34 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "bucket-tofu-ssilva2026"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
-# Configura el proveedor de AWS
 provider "aws" {
   region = "us-east-1"
 }
 
-# Creamos un recurso gratis para probar: Un grupo de logs vacío
+# 1. Creamos el contenedor S3 para el estado remoto
+resource "aws_s3_bucket" "estado_tofu" {
+  bucket        = "bucket-tofu-ssilva2026" 
+  force_destroy = true # Permite borrarlo fácilmente con tofu destroy luego
+}
+
+# 2. Bloqueamos el acceso público al bucket por seguridad
+resource "aws_s3_bucket_public_access_block" "bloqueo_s3" {
+  bucket = aws_s3_bucket.estado_tofu.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# 3. Mantenemos nuestro grupo de logs de prueba anterior
 resource "aws_cloudwatch_log_group" "mi_log_de_pruebas" {
   name              = "/mis-pruebas/primer-log"
   retention_in_days = 1
